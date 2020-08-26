585aa044c1066772351c9bf6f5cae81e4facd674
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-08-26 18:53:48 +0200

    ajout de la fonction supprimer collegue

--- src/main/java/dev/collegues/controler/CollegueCtrl.java
+++ src/main/java/dev/collegues/controler/CollegueCtrl.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -105,5 +106,17 @@ public class CollegueCtrl {
 		return collegueDto;
 	}
 	
+	@DeleteMapping(value="collegues/{uuidString}")
+	public ResponseEntity<?> deleteCollegue (@PathVariable String uuidString){
+		
+		UUID matricule = null;
+		try{
+			matricule = UUID.fromString(uuidString);
+		} catch(IllegalArgumentException e) {
+			new IllegalArgumentException (e.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Uuid invalide");
+		}
+		return collegueService.delete(matricule);
+	}
 
 }
